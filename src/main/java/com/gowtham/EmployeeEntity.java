3192068a9ec5f1d31009e5c976ae37d2ce59006c
Gowtham Behara <gothsiddu@gmail.com>
2019-05-22 14:49:04 +0530

    Add getter and setter methods of EmployeeEntity attribs

--- src/main/java/com/gowtham/EmployeeEntity.java
+++ src/main/java/com/gowtham/EmployeeEntity.java
@@ -26,4 +26,36 @@ public class EmployeeEntity implements Serializable {
 
     @Column(name = "LASTNAME", unique = false, nullable = false, length = 100)
     private String lastName;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
