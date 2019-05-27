f94088df8bb69f7732b02e2787ec57413223c9de
Gowtham Behara <gothsiddu@gmail.com>
2019-05-27 11:22:59 +0530

    Modularize insert employee

--- src/main/java/com/gowtham/App.java
+++ src/main/java/com/gowtham/App.java
@@ -1,22 +1,30 @@
 package com.gowtham;
 
 import org.hibernate.Session;
+import org.hibernate.exception.ConstraintViolationException;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
 
-        EmployeeEntity employeeEntity = new EmployeeEntity();
-        employeeEntity.setEmail("gothsiddu@gmail.com");
-        employeeEntity.setFirstName("demo");
-        employeeEntity.setLastName("user");
+        insertEmployeeEntity(session);
 
-        session.save(employeeEntity);
-
-        session.getTransaction().commit();
         HibernateUtil.shutDown();
+    }
+
+    static void insertEmployeeEntity(Session session) {
+        try {
+            EmployeeEntity employeeEntity = new EmployeeEntity();
+            employeeEntity.setEmail("gothsiddu@gmail.com");
+            employeeEntity.setFirstName("Gowtham");
+            employeeEntity.setLastName("Behara");
+
+            session.save(employeeEntity);
+
+            session.getTransaction().commit();
+        } catch (ConstraintViolationException cve) {
+            System.out.println(cve.getConstraintName() + ", " + cve.getMessage());
+        }
     }
 }
